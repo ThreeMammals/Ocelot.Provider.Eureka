fe933a67ece6109be86c4f2590c73fd8da966d06
Tom Pallister <tom@threemammals.com>
2018-08-12 14:19:36 +0100

    initial copy from archetype

---
+++ src/Ocelot.Package.Archetype/OcelotBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Ocelot.Package.Archetype
+{
+    using DependencyInjection;
+
+    public static class OcelotBuilderExtensions
+    {
+        public static IOcelotBuilder AddSomething(this IOcelotBuilder builder)
+        {
+            return builder;
+        }
+    }
+}
